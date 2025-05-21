5a7bcee54f779d84662077f6f951c813db4a3cf1
AmandaPinheiro <amandadejesuspinheiro@outlook.com>
2025-05-21 19:03:16 -0300

    finalização da aula da agenda 14

--- MauiAppHotel1/App.xaml.cs
+++ MauiAppHotel1/App.xaml.cs
@@ -1,7 +1,42 @@
-﻿namespace MauiAppHotel1
+﻿using MauiAppHotel1.Models;
+
+namespace MauiAppHotel1
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>()
+        {
+
+            new Quarto()
+            {
+                Descricao = "Suite Super Luxo",
+                ValorDiariaAdulto = 110.00,
+                ValorDiariaCrianca = 55.00,
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suite Luxo",
+                ValorDiariaAdulto = 80.00,
+                ValorDiariaCrianca = 40.00,
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suite Single",
+                ValorDiariaAdulto = 50.00,
+                ValorDiariaCrianca = 25.00,
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suite Crise",
+                ValorDiariaAdulto = 25.00,
+                ValorDiariaCrianca = 12.5
+            }
+
+        };
+
         public App()
         {
             InitializeComponent();
--- MauiAppHotel1/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel1/Views/ContratacaoHospedagem.xaml.cs
@@ -2,9 +2,25 @@ namespace MauiAppHotel1.Views
 {
     public partial class ContratacaoHospedagem : ContentPage
     {
+        App PropriedadesApp;
+
+
         public ContratacaoHospedagem()
         {
             InitializeComponent();
+
+            PropriedadesApp = (App)Application.Current;
+
+            pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+            dtpck_chekin.MinimumDate = DateTime.Today;
+            dtpck_chekin.MaximumDate = DateTime.Today.AddMonths(3);
+            dtpck_chekin.Date = DateTime.Today;
+
+            dtpck_chekout.MinimumDate = dtpck_chekin.Date.AddDays(1);
+            dtpck_chekout.MaximumDate = dtpck_chekin.Date.AddMonths(6);
+            dtpck_chekout.Date = DateTime.Today;
+
         }
 
         private async void sobreClick(object sender, EventArgs e)
@@ -23,6 +39,16 @@ namespace MauiAppHotel1.Views
             {
                 DisplayAlert("Ops", ex.Message, "OK");
             }
+        }
+
+        private void dtpck_chekin_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DatePicker elemento = sender as DatePicker;
+
+            DateTime data_selecionada_do_checkin = elemento.Date;
+                      
+            dtpck_chekout.MinimumDate = data_selecionada_do_checkin.AddDays(1);
+            dtpck_chekout.MaximumDate = data_selecionada_do_checkin.AddMonths(6);
         }
     }
 }
